64ce736c720dfc58308c0c484b52961140e4ead1
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-05-07 15:18:41 +0200

    php.packages.psysh: switch to `buildComposerProject2`

--- pkgs/development/php-packages/psysh/default.nix
+++ pkgs/development/php-packages/psysh/default.nix
@@ -1,22 +1,53 @@
 {
   fetchFromGitHub,
+  fetchurl,
   lib,
   php,
 }:
 
-php.buildComposerProject (finalAttrs: {
+let
   pname = "psysh";
-  version = "0.11.21";
+  version = "0.12.3";
 
   src = fetchFromGitHub {
     owner = "bobthecow";
     repo = "psysh";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-YuBn4mrgOzGeMGfGcyZySAISmQdv3WRGn91PRozyxdI=";
+    rev = "v${version}";
+    hash = "sha256-v2UAhxnfnVwA05sxcqMU5vmQcwlBcc901PYJqYf+pCw=";
   };
 
-  composerLock = ./composer.lock;
-  vendorHash = "sha256-FZFeO7UiVssxTf0JX6wdjrAE+jucYnfQJA1eOng39lQ=";
+  composerLock = fetchurl {
+    name = "composer.lock";
+    url = "https://github.com/bobthecow/psysh/releases/download/v${version}/composer-v${version}.lock";
+    hash = "sha256-ur6mzla3uXeFL6aEHAPdpxGdvcgzOgTLW/CKPbNqeCg=";
+  };
+in
+php.buildComposerProject2 (finalAttrs: {
+  inherit
+    pname
+    version
+    composerLock
+    src
+    ;
+
+  composerRepository = php.mkComposerRepository2 {
+    inherit
+      src
+      version
+      pname
+      composerLock
+      ;
+
+    preBuild = ''
+      export COMPOSER_ROOT_VERSION="${version}"
+      composer config platform.php 7.4
+      composer require --no-update symfony/polyfill-iconv symfony/polyfill-mbstring
+      composer require --no-update --dev roave/security-advisories:dev-latest
+      composer update --lock --no-install
+    '';
+
+    vendorHash = "sha256-DNsHGNQvUgo9T3IX9PZMyQCWT2/2P68WDzSxAGfXDPg=";
+  };
 
   meta = {
     changelog = "https://github.com/bobthecow/psysh/releases/tag/v${finalAttrs.version}";
